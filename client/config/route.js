4ff770cd3fe050d1892fc96e348255d72e511d3d
Chyld Medford <chyld.medford@gmail.com>
2015-10-27 14:11:09 -0700

    about

--- client/config/route.js
+++ client/config/route.js
@@ -2,5 +2,6 @@ export default function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-  .state('home', {url: '/', templateUrl: '/views/general/home.html'});
+  .state('home', {url: '/', templateUrl: '/views/general/home.html'})
+  .state('about', {url: '/about', templateUrl: '/views/general/about.html'});
 }
