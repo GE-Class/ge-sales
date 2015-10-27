0910ca11101a3168295bdb874d951551c5b7f8d4
Chyld Medford <chyld.medford@gmail.com>
2015-10-27 14:02:48 -0700

    routes

---
+++ client/app.js
@@ -0,0 +1,7 @@
+import angular from 'angular';
+import router from 'angular-ui-router';
+import config from './config/route.js';
+
+let app = angular.module('geSales', ['ui.router']);
+
+app.config(config);
---
+++ client/config/route.js
@@ -0,0 +1,6 @@
+export default function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {url: '/', templateUrl: '/views/general/home.html'});
+}
